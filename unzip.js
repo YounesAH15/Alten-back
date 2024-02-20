041ff49438a6bb159437e5a20c31f8d8eff3684a
YounesAH15 Harrat <younesharrat5896@gmail.com>
2024-02-20 14:59:53 +0000

    dir

--- unzip.js
+++
@@ -1,7 +0,0 @@
-const zl = require("zip-lib");
-
-zl.extract("/workspace/Zip/alten-shop-back-products.zip", "./unzipped/").then(function () {
-    console.log("done");
-    }, function (err) {
-        console.log(err);
-        });
